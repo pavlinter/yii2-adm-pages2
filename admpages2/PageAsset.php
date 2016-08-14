6e469968a7980f8d299e0e282f581ddd291f0d28
Pavel <pavlinter@gmail.com>
2016-08-14 13:00:12 +0300

    @version 0.1.0

--- admpages2/PageAsset.php
+++ admpages2/PageAsset.php
@@ -4,7 +4,7 @@
  * @package yii2-adm-pages2
  * @author Pavels Radajevs <pavlinter@gmail.com>
  * @copyright Copyright &copy; Pavels Radajevs <pavlinter@gmail.com>, 2015
- * @version 0.0.2
+ * @version 0.1.0
  */
 
 namespace pavlinter\admpages2;

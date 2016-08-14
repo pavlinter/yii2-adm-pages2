48bee3ac8aee74b3f5690e8787b962d1a5b335f8
Pavel <pavlinter@gmail.com>
2016-08-14 17:30:10 +0300

    fix params

--- admpages2/views/default/page.php
+++ admpages2/views/default/page.php
@@ -3,7 +3,7 @@
 /* @var $this yii\web\View */
 /* @var $model \pavlinter\admpages2\models\Page */
 
-$this->params['breadcrumbs'][] = $model->name;
+Yii::$app->params['breadcrumbs'][] = $model->name;
 ?>
 <div class="adm-pages-layout-page">
     <h1><?= $model->title ?></h1>
